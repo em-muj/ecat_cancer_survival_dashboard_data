da6e5ec6be98908b163f969c4e8c05e65cc7f98c
Emma Muijen <emma.muijen@dhsc.gov.uk>
2024-11-22 11:20:20 +0000

    Development of cleaning adult survival data. Still yet to be automated.

--- Background Processes/02_adult_data_prep.R
+++ Background Processes/02_adult_data_prep.R
@@ -7,8 +7,10 @@
 #   (a) Table 1 - Gender & Age
 #   (b) Table 2 - Gender & Stage
 #   (c) Table 3 - Gender & Deprivation
-#   (d) Table 4 - Gender & Country
-#   (e) Table 5 - Gender & Region
+# 2. Trends between recent and previous datasets
+#   (a) Trends for cancer sites
+#   (b) Trends for cancer sites by stage
+
 
 # Cleaning column names of all tables
 for (i in 1:5){
@@ -34,16 +36,37 @@ for (i in 1:5){
 
 # (a) Table 1 - Gender & Age ----------------------------------------------
 
+# Isolating relevant data
 adult_cancer_survival_rcnt_table_1_ch1 <- adult_cancer_survival_rcnt_table_1 %>%
   dplyr::select(cancer_site, gender, age_at_diagnosis_years, standardisation_type, 
                 years_since_diagnosis, patients, net_survival_percent) %>%
-  dplyr::filter(gender == "Persons" & standardisation_type == "Age-standardised (5 age groups)" &
-                  years_since_diagnosis %in% c(1, 5)) # SEPARATE THESE INTO TABLES WITH 1 AND 5 YR SURVIVAL?
+  dplyr::filter((!("Persons" %in% gender) | gender == "Persons") & standardisation_type == "Age-standardised (5 age groups)" &
+                  years_since_diagnosis %in% c(1, 5, 10)) 
+
+# Rearranging data to have columns with survival for 1 and 5 years.
+adult_cancer_survival_rcnt_table_1_ch2 <- adult_cancer_survival_rcnt_table_1_ch1 %>%
+  dplyr::group_by(cancer_site) %>%
+  dplyr::mutate(
+    one_year_survival = dplyr::case_when(
+      years_since_diagnosis == 1 ~ as.numeric(net_survival_percent)
+    ),
+    five_year_survival = dplyr::case_when(
+      years_since_diagnosis == 5 ~ as.numeric(net_survival_percent)
+    ),
+    ten_year_survival = dplyr::case_when(
+      years_since_diagnosis == 10 ~ as.numeric(net_survival_percent)
+    )
+  ) %>%
+  dplyr::summarise(one_year_survival = ifelse(sum(one_year_survival, na.rm = T) == 0, NA, sum(one_year_survival, na.rm = T)),
+                   five_year_survival = ifelse(sum(five_year_survival, na.rm = T) == 0, NA, sum(five_year_survival, na.rm = T)),
+                   ten_year_survival = ifelse(sum(ten_year_survival, na.rm = T) == 0, NA, sum(ten_year_survival, na.rm = T))) %>%
+  ungroup() %>%
+  dplyr::select(cancer_site, one_year_survival, five_year_survival, ten_year_survival)
 
 
 # (b) Table 2 - Gender & Stage-------------------------------------------------------------
 
-# Need a table that has each cancer type by each stage
+# Cleaning data
 adult_cancer_survival_rcnt_STAGE <- adult_cancer_survival_rcnt_table_2 %>%
   dplyr::select(cancer_site, gender, age_at_diagnosis, stage_at_diagnosis, standardisation_type,
                 years_since_diagnosis, patients, net_survival_percent) %>%
@@ -54,4 +77,124 @@ adult_cancer_survival_rcnt_STAGE <- adult_cancer_survival_rcnt_table_2 %>%
   dplyr::group_by(cancer_site) %>%
   filter(!("Persons" %in% gender) | gender == "Persons") %>%
   ungroup()
-# NEED TO WRITE SOME CODE TO EXCLUDE GENDER SPECIFYING WHEN PERSONS PRESENT
+
+# Specifying Stage 1/2 and 3/4
+adult_cancer_survival_rcnt_STAGE2 <- adult_cancer_survival_rcnt_STAGE %>%
+  dplyr::mutate(
+    stage_diagnosed = dplyr::case_when(
+      stage_at_diagnosis %in% c("1","2") ~ "Stage 1 or 2",
+      stage_at_diagnosis %in% c("3", "4") ~ "Stage 3 or 4",
+      stage_at_diagnosis == "Unstageable" ~ "Unstageable",
+      TRUE ~ NA
+    )
+  ) %>%
+  dplyr::group_by(cancer_site, stage_diagnosed) %>%
+  dplyr::mutate(
+    one_year_survival = dplyr::case_when(
+      years_since_diagnosis == 1 ~ as.numeric(net_survival_percent)
+    ),
+    five_year_survival = dplyr::case_when(
+      years_since_diagnosis == 5 ~ as.numeric(net_survival_percent)
+    ),
+    ten_year_survival = dplyr::case_when(
+      years_since_diagnosis == 10 ~ as.numeric(net_survival_percent)
+    )
+  ) %>%
+  dplyr::summarise(one_year_survival = ifelse(sum(one_year_survival, na.rm = T) == 0, NA, 100*sum(one_year_survival, na.rm = T)/200),
+                   five_year_survival = ifelse(sum(five_year_survival, na.rm = T) == 0, NA, 100*sum(five_year_survival, na.rm = T)/200),
+                   ten_year_survival = ifelse(sum(ten_year_survival, na.rm = T) == 0, NA, 100*sum(ten_year_survival, na.rm = T)/200)) %>%
+  ungroup() %>%
+  dplyr::select(cancer_site, stage_diagnosed, one_year_survival, five_year_survival, ten_year_survival) %>%
+  dplyr::filter(!is.na(stage_diagnosed))
+
+# (c) Table 3 - Gender & Deprivation -------------------------------------------------------------
+
+# Cleaning data
+adult_cancer_survival_rcnt_DEPRIVATION <- adult_cancer_survival_rcnt_table_3 %>%
+  dplyr::select(cancer_site, gender, imd_quintile, standardisation_type,
+                years_since_diagnosis,  net_survival_percent) %>%
+  dplyr::filter(standardisation_type == "Age-standardised (5 age groups)" & 
+                  years_since_diagnosis %in% c(1,5)) %>% 
+  dplyr::arrange(cancer_site, years_since_diagnosis) %>%
+  dplyr::group_by(cancer_site) %>%
+  filter(!("Persons" %in% gender) | gender == "Persons") %>%
+  ungroup()
+
+# Grouping survival rates by cancer and deprivation quintile
+adult_cancer_survival_rcnt_DEPRIVATION_2 <- adult_cancer_survival_rcnt_DEPRIVATION %>%
+  dplyr::group_by(cancer_site, imd_quintile) %>%
+  dplyr::mutate(
+    one_year_survival = dplyr::case_when(
+      years_since_diagnosis == 1 ~ as.numeric(net_survival_percent)
+    ),
+    five_year_survival = dplyr::case_when(
+      years_since_diagnosis == 5 ~ as.numeric(net_survival_percent)
+    ),
+    ten_year_survival = dplyr::case_when(
+      years_since_diagnosis == 10 ~ as.numeric(net_survival_percent)
+    )
+  ) %>%
+  dplyr::summarise(one_year_survival = ifelse(sum(one_year_survival, na.rm = T) == 0, NA, sum(one_year_survival, na.rm = T)),
+                   five_year_survival = ifelse(sum(five_year_survival, na.rm = T) == 0, NA, sum(five_year_survival, na.rm = T)),
+                   ten_year_survival = ifelse(sum(ten_year_survival, na.rm = T) == 0, NA, sum(ten_year_survival, na.rm = T))) %>%
+  ungroup() %>%
+  dplyr::select(cancer_site, imd_quintile, one_year_survival, five_year_survival, ten_year_survival)
+
+
+######################################################
+# 2. Trends between recent and previous datasets
+######################################################
+
+
+# (a) Trends for cancer sites ---------------------------------------------
+
+# Cleaning table 1 from previous data
+
+adult_cancer_survival_prev_table_1_clean <- adult_cancer_survival_prev_table_1 %>%
+  dplyr::select(cancer_site, gender, age_at_diagnosis, standardisation_type, 
+                x1_year_survival_percent, x5_year_survival_percent) %>%
+  dplyr::filter((!("Persons" %in% gender) | gender == "Persons") & standardisation_type == "Age-standardised (5 gps)") %>%
+  dplyr::select(cancer_site, x1_year_survival_percent, x5_year_survival_percent) %>%
+  dplyr::rename(one_year_survival_prev = x1_year_survival_percent, five_year_survival_prev = x5_year_survival_percent)
+
+# Joining previous and recent datasets
+adult_cancer_survival_trends <- 
+  dplyr::inner_join(adult_cancer_survival_rcnt_table_1_ch2, adult_cancer_survival_prev_table_1_clean)
+
+# Assessing percentage change
+adult_cancer_survival_trends_2 <- adult_cancer_survival_trends %>%
+  dplyr::mutate(
+    prcnt_chng_one_yr = 100*(one_year_survival - as.numeric(one_year_survival_prev))/as.numeric(one_year_survival_prev),
+    prcnt_chng_five_yr = 100*(five_year_survival - as.numeric(five_year_survival_prev))/as.numeric(five_year_survival_prev),
+    #prcnt_chng_ten_yr = 100*(ten_year_survival - as.numeric(ten_year_survival_prev))/as.numeric(ten_year_survival_prev)
+  ) %>%
+  dplyr::select(cancer_site, prcnt_chng_one_yr, prcnt_chng_five_yr)#, prcnt_chng_ten_yr)
+
+
+
+# (b) Trends for cancer sites by stage ------------------------------------
+
+# Cleaning table 2 from previous data
+
+adult_cancer_survival_prev_table_2_clean <- adult_cancer_survival_prev_table_2 %>%
+  dplyr::group_by(cancer_site) %>%
+  dplyr::filter((!("Persons" %in% gender) | gender == "Persons")) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate_at(c('x1_year_stage_1', 'x1_year_stage_2', 'x1_year_stage_3',
+                     'x1_year_stage_4', 'x5_year_stage_1', 'x5_year_stage_2',
+                     'x5_year_stage_3', 'x5_year_stage_4'), as.numeric) %>%
+  dplyr::mutate(
+    stage_1_2_survival_1yr = 100*(x1_year_stage_1 + x1_year_stage_2)/200,
+    stage_3_4_survival_1yr = 100*(x1_year_stage_3 + x1_year_stage_4)/200,
+    stage_1_2_survival_5yr = 100*(x5_year_stage_1 + x5_year_stage_2)/200,
+    stage_3_4_survival_5yr = 100*(x5_year_stage_3 + x5_year_stage_4)/200,
+  ) %>%
+  dplyr::select(cancer_site, stage_1_2_survival_1yr, stage_3_4_survival_1yr,
+                stage_1_2_survival_5yr, stage_3_4_survival_5yr)
+
+# Structuring similarly to the recent data table
+adult_cancer_survival_prev_table_2_clean_2 <- adult_cancer_survival_prev_table_2_clean %>%
+  dplyr::group_by(cancer_site) %>%
+  dplyr::summarise(
+    stage_diagnosis = 
+  )
---
+++ MAIN SCRIPT.R
@@ -0,0 +1,23 @@
+# Emma Muijen
+# 21 November 2024
+# Generating data for survival dashboard
+
+# USER SHOULD ENSURE THEY HAVE FILLED IN THE USER INPUTS SCRIPT. THEY CAN THEN HIGHLIGHT ALL
+# (CTRL + A) AND CLICK "RUN" IN THE TOP RIGHT HAND CORNER OF THIS WINDOW.
+
+# ToC:
+# 1. Downloading Data
+# 2. Preparing Adult Survival Data
+# 3. Preparing Childhood Cancer Survival Data
+# 4. Preparing Cancer Survival: Index for sub-ICBs Data
+
+
+# 1. Downloading Data --------------------------------------------------------
+
+source("Background Processes/01_downloading_data.R")
+
+
+# 2. Preparing Adult Survival Data -------------------------------------------
+
+source("Background Processes/02_adult_data_prep.R")
+
